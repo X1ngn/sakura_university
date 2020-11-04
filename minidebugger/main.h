36a1ab419d84a6856c898eab1c3eabab1f7a7739
X1ngn <55945184+X1ngn@users.noreply.github.com>
2020-11-04 11:32:24 +0800

    Create main.h

---
+++ minidebugger/main.h
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <sstream>
+
+
+#include <unistd.h>
+#include <stdio.h>
+
+#include <utility>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/ptrace.h>
+#include <sys/wait.h>
+#include <sys/user.h>
